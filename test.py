0fb7b48eca982e014a88dc3854ac8c00458c5033
Johannes Kerger <49689041+JohannesK71083@users.noreply.github.com>
2025-03-10 07:42:20 +0100

    -

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from os import environ
-with open(environ["GITHUB_ENV"], "a") as f:
-    f.write("TEST1<<EOF\ntest\ntest2\ntest3\nEOF")
+from typeguard import check_type
+
+print(check_type('hello', str))
